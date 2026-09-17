c46cd7327430d3857131eecb3c354cfc92917040
mrtanandy88 <mrtanandy88@gmail.com>
2026-09-17 20:34:36 +0800

    Update service worker cache for V2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='airport-transfer-v1';
-const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='airport-transfer-v2';
+const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js','./js/firebase-config.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
