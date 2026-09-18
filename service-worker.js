8b1e633987518e2ac022622f7b441305de5b5c5d
mrtanandy88 <mrtanandy88@gmail.com>
2026-09-18 19:14:03 +0800

    Update service worker cache for V18

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='airport-transfer-v23';
-const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js','./js/language-picker.js','./js/customer-driver-card.js','./js/driver-auth-ui.js','./js/firebase-config.js'];
+const CACHE='airport-transfer-v24';
+const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js','./js/language-picker.js','./js/customer-driver-card.js','./js/driver-auth-ui.js','./js/map-picker.js','./js/firebase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
