26f7e190bc409dda407a8360ed3d8ba9d35d1dc2
mrtanandy88 <mrtanandy88@gmail.com>
2026-09-19 21:12:31 +0800

    Refresh PWA cache for latest trip status fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='airport-transfer-v29';
-const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js?v=30','./js/language-picker.js','./js/customer-driver-card.js','./js/driver-auth-ui.js','./js/firebase-config.js'];
+const CACHE='airport-transfer-v30';
+const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js?v=31','./js/language-picker.js','./js/customer-driver-card.js','./js/driver-auth-ui.js','./js/firebase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
