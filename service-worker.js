8d70853bc7eeeec84235bf895f65a4544da2496c
mrtanandy88 <mrtanandy88@gmail.com>
2026-09-25 12:36:33 +0800

    Refresh app after GPS profile fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='airport-transfer-v54';
+const CACHE='airport-transfer-v55';
 const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app-v37.js','./js/admin-drivers.js','./js/language-picker.js','./js/customer-driver-card.js','./js/driver-auth-ui.js','./js/firebase-config.js','./js/notifications-v1.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
